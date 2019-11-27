1b6191f1017c5f35aa72dccbb4d704a4daded462
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2019-11-27 17:44:10 +0100

    add mongoose schema

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
+mongoose.connect(process.env.DB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 const app = express();
 
-app.get('/', (req, res) => {
-    
-})
+app.get('/', (req, res) => {});
 
-app.listen(3000, () => console.log('app listening on 3000'))
+app.listen(3000, () => console.log('app listening on 3000'));
