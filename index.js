e44ccb3b6fe4e232d8e8580e4cf79a5823e89dc3
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2019-11-27 18:18:16 +0100

    use redis

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const redis = require('redis');
 
 const Cat = require('./models/cat');
 require('dotenv').config();
+
+const client = redis.createClient(6379, '127.0.0.1');
 
 mongoose.connect(process.env.DB_URL, {
   useNewUrlParser: true,
@@ -13,7 +16,33 @@ const app = express();
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  Cat.find()
+  client.get('http://localhost:3000/', (err, cats) => {
+    console.log(err);
+    if (err) {
+      throw err;
+    }
+    if (cats) {
+        console.log(cats)
+      return res.json({ cats: JSON.parse(cats) });
+    } else {
+      console.log('aaa');
+      Cat.find()
+        .then(data => {
+          client.set('http://localhost:3000/', JSON.stringify(data), err => {
+            if (err) {
+              throw err;
+            }
+          });
+          res.json({ cats: data });
+        })
+        .catch(err => res.json({ err }));
+    }
+  });
+});
+
+app.get('/:id', (req, res) => {
+  const { id } = req.params;
+  Cat.find({ _id: id })
     .then(data => res.json({ cats: data }))
     .catch(err => res.json({ err }));
 });
