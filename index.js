0084adff1b53d0e8d161152a5f5d41acc3537b41
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2019-11-27 17:56:21 +0100

    add endpoints

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+
+const Cat = require('./models/cat');
 require('dotenv').config();
 
 mongoose.connect(process.env.DB_URL, {
@@ -8,7 +10,24 @@ mongoose.connect(process.env.DB_URL, {
 });
 
 const app = express();
+app.use(express.json());
 
-app.get('/', (req, res) => {});
+app.get('/', (req, res) => {
+  Cat.find()
+    .then(data => res.json({ cats: data }))
+    .catch(err => res.json({ err }));
+});
+
+app.post('/', (req, res) => {
+  const { name, age, type } = req.body;
+  const catsData = {
+    name,
+    age,
+    type
+  };
+  Cat.create(catsData)
+    .then(data => res.json({ cats: data }))
+    .catch(err => res.json({ err }));
+});
 
 app.listen(3000, () => console.log('app listening on 3000'));
